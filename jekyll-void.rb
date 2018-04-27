fbb9fa2f9956ce85476633ac049cf78e8aaaff01
Philipp Serr <episource@gmx.de>
2018-04-27 19:40:09 +0200

    Add plugin providing tags `void` and `bang`

---
+++ jekyll-void.rb
@@ -0,0 +1,48 @@
+module Jekyll
+  module Void
+    BANGS_KEY = Object.new
+  
+    class VoidBlock < Liquid::Block
+    @@level = 0
+    
+      def initialize(tag_name, text, tokens)
+         super
+      end
+    
+      def render(context)
+        bangs = context.find_variable(BANGS_KEY)
+        if bangs.nil?
+            bangs = []
+            context[BANGS_KEY] = bangs
+        end
+        
+        super
+        rendered = bangs.join("")
+        bangs.clear
+        
+        rendered
+      end
+    end
+    class BangBlock < Liquid::Block
+        @@level = 0
+    
+      def initialize(tag_name, text, tokens)
+         super
+      end
+    
+      def render(context)
+        rendered = super
+        
+        bangs = context.find_variable(BANGS_KEY)
+        unless bangs.nil?
+            bangs.push(rendered)
+        end
+        
+        rendered
+      end
+    end
+  end
+end
+
+Liquid::Template.register_tag('void', Jekyll::Void::VoidBlock)
+Liquid::Template.register_tag('bang', Jekyll::Void::BangBlock)
